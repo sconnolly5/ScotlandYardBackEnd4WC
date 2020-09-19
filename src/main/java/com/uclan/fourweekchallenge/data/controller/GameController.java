c5bcd7ce459a55ae89ee2d8ec258626370b55b6e
sconnolly5 <sconnolly5@uclan.ac.uk>
2020-09-19 01:29:56 +0100

    Added game descriptors to the API
    Game descriptors are now available for games, under /games/descriptors. These are much more readable, and return information about a game rather than relying on the user to send off multiple requests to acquire the information.

--- src/main/java/com/uclan/fourweekchallenge/data/controller/GameController.java
+++ src/main/java/com/uclan/fourweekchallenge/data/controller/GameController.java
@@ -1,14 +1,19 @@
 package com.uclan.fourweekchallenge.data.controller;
 
+import com.google.common.collect.Lists;
 import com.uclan.fourweekchallenge.data.entity.Game;
+import com.uclan.fourweekchallenge.data.entity.GameDescriptor;
 import com.uclan.fourweekchallenge.data.entity.Player;
 import com.uclan.fourweekchallenge.data.repository.GameRepository;
+import com.uclan.fourweekchallenge.data.repository.MapRepository;
 import com.uclan.fourweekchallenge.data.repository.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/games")
@@ -19,12 +24,32 @@ public class GameController {
 
     @Autowired
     private GameRepository gameRepository;
-    
+
+    @Autowired
+    private MapRepository mapRepository;
+
     @GetMapping
     public Iterable<Game> getGames() {
         return this.gameRepository.findAll();
     }
-    
+
+    @GetMapping("/descriptors")
+    public Map<Long, GameDescriptor> getGameDescriptors() {
+        Map<Long, GameDescriptor> results = new HashMap<>();
+        Iterable<Game> games = this.gameRepository.findAll();
+        games.forEach(game -> {
+            GameDescriptor gameDescriptor = new GameDescriptor();
+            gameDescriptor.setGameId(game.getGameId());
+            int numPlayers = Lists.newArrayList(playerRepository.findByGameId(game.getGameId())).size();
+            gameDescriptor.setNumPlayers(numPlayers);
+
+            Optional<com.uclan.fourweekchallenge.data.entity.Map> map = mapRepository.findById(game.getMapId());
+            map.ifPresent(value -> gameDescriptor.setMapName((value.getName())));
+            results.put(gameDescriptor.getGameId(), gameDescriptor);
+        });
+        return results;
+    }
+
     @GetMapping("/create")
     public Game createGame() {
         Iterable<Game> games = this.gameRepository.findAll();
@@ -33,7 +58,7 @@ public class GameController {
         this.gameRepository.save(game);
         return game;
     }
-    
+
     @GetMapping("/destroy")
     public Map<String, Boolean> destroyGame(@RequestParam long gameId) {
         Game game = gameRepository.findByGameId(gameId);
